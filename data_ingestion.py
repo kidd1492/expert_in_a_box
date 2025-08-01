c962d68c466d1b4c73ce8b2f09a3851b777a38fb
kidd1492 <chris.wr1492@gmail.com>
2025-08-01 11:30:44 -0400

    added log

--- data_ingestion.py
+++ data_ingestion.py
@@ -2,6 +2,9 @@ from embedding import load_or_create_vector_store
 import fitz  # PyMuPDF
 import re
 from chunking import chunk_pdf_text
+from log_handler import app_logger, project_logger
+
+
 
 def load_pdf(filepath: str) -> str:
     """Load and clean text from a single PDF file."""
@@ -18,9 +21,10 @@ def load_pdf(filepath: str) -> str:
 
 
 
-pdf_path = "C:/Users/chris/Desktop/AI_research/machine_learning.pdf"
+pdf_path = "C:/Users/chris/Desktop/AI_research/intro_to_ML.pdf"
+project_logger.info(pdf_path)
 text = load_pdf(pdf_path)
-chunks = chunk_pdf_text(text, chunk_size=300, chunk_overlap=30)
+chunks = chunk_pdf_text(text, chunk_size=500, chunk_overlap=50)
 
 # Optional preview
 for i, chunk in enumerate(chunks[:5]):
