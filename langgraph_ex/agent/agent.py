4681770f832f5157a0746ea6dd6b6564850823a8
kidd1492 <chris.wr1492@gmail.com>
2025-08-20 11:07:09 -0400

    new files

---
+++ langgraph_ex/agent/agent.py
@@ -0,0 +1,74 @@
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage
+from langgraph.graph import MessagesState, START, END, StateGraph
+from langgraph.prebuilt import tools_condition, ToolNode
+
+
+class State(MessagesState):
+    pass
+
+
+def multipy(a:int, b:int) -> int:
+    '''this function multipies two numbers'''
+    return a * b
+
+
+def add(a:int, b:int) -> int:
+    '''this function adds two numbers'''
+    return a + b
+
+
+def subtract(a:int, b:int) -> int:
+    '''this function subtracts two numbers'''
+    return a - b
+
+
+llm = ChatOllama(model='personal:3b')
+
+tools = [multipy, add, subtract]
+llm_with_tools = llm.bind_tools(tools=tools)
+
+sys_msg = SystemMessage(content="""You are a helpful assistant tasked with performing arithmatic on a set of inputs.
+                         you have tools = [multipy, add, subtract] arg a:int, b:int""")
+
+
+def assistant(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+'''
+def should_continue(state: MessagesState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    return "continue" if getattr(last_message, "tool_calls", None) else "end"
+'''
+
+builder = StateGraph(MessagesState)
+
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools=tools))
+
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges("assistant", tools_condition)
+
+'''
+builder.add_conditional_edges(
+    "assistant",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END,
+    },
+)
+'''
+builder.add_edge("tools", "assistant")
+
+app = builder.compile()
+
+with open("graph2.png", "wb") as f:
+    f.write(app.get_graph().draw_mermaid_png())
+
+
+messages = HumanMessage(content="add 3 plus 3, then multipy the sum by 2. and finally subtract 1?")
+messages = app.invoke({"messages":messages})
+for m in messages["messages"]:
+    m.pretty_print()
