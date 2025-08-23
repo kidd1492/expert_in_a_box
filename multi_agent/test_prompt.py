90f1b245112e03c99e5eda08b5a7a204a6ebb9b1
kidd1492 <chris.wr1492@gmail.com>
2025-08-23 11:02:32 -0400

    started working on multi agent.

--- multi_agent/test_prompt.py
+++ multi_agent/test_prompt.py
@@ -1,44 +1,84 @@
-from typing import Annotated, Sequence, TypedDict
-from langchain_core.messages import BaseMessage
-from langgraph.graph.message import add_messages
 from langchain_ollama import ChatOllama
-from langgraph.graph import START, StateGraph, END
+from langgraph.graph import START, StateGraph, END, MessagesState
 from ReAct_agent import running_agent
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langgraph.graph.message import RemoveMessage
 
-class AgentState(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], add_messages]
-    system_prompt: str
-    topic: str
-    question: str
-    research_summary: str
+
+class AgentState(MessagesState):
+    pass
 
 model = ChatOllama(model="qwen2.5:3b")
 
+def human_node(state: AgentState) -> AgentState:
+    user_input = input("Enter Question: ")
+    state['messages'] = state['messages'] + [HumanMessage(content=user_input)] 
+    return state
+
+
+sys_msg = SystemMessage(content="""You are lesson planner. 
+                        - Given the users topic: 
+                        - ask 3 to 5 questions to better understand what the user would like to learn.
+                        - once you have the information make a lesson plain to teach the topic""")
+
+
 
 def chat_agent(state: AgentState) -> AgentState:
-    result = model.invoke(f"{state['system_prompt']} {state['topic']}")
-    state['question'] = result.content
+    result = model.invoke([sys_msg] + state["messages"]) 
+    state['messages'] = state['messages'] + [AIMessage(content=result.content)] 
     print(result.content)
     return state
 
 
-graph = StateGraph(AgentState)
-graph.add_node('chatbot', chat_agent)
-graph.add_node('researcher', running_agent)
+def research_node(state: AgentState) -> AgentState:
+    research_msg = SystemMessage(content="""Take the conversation and make research plan.
+                                 The plan should include. 
+                                 - 2 search terms related to the topic for youtube video tutorials
+                                 - if the outline mentions langgraph -2 search terms for the vectorstore
+                                 - and one overall themed wikipedia search term. 
+                                 reponse: return only in a json format with-  research topic:, youtube search terms:, vectorstore terms: wikisearch term:""")
+    result = model.invoke([research_msg] + state["messages"])
+    state['messages'] = state['messages'] + [AIMessage(content=result.content)]   
+    print(result.content)
+    return state
 
-graph.add_edge(START, 'chatbot')
-graph.add_edge('chatbot', 'researcher')
-graph.add_edge('researcher', END)
+
+def should_continue(state:AgentState):
+    if state['messages'][-1].content == "finished":
+        return "delete"
+    else:
+        return "chat"
+         
+
+def delete_messages(state:AgentState):
+    delete_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-2]]
+    return {"messages": delete_messages}
+
+
+graph = StateGraph(AgentState)
+
+graph.add_node("human_node", human_node)
+graph.add_node('chatbot', chat_agent)
+graph.add_node("research_node", research_node)
+graph.add_node("delete_messages", delete_messages)
+
+graph.add_edge(START, 'human_node')
+graph.add_conditional_edges(
+    "human_node",
+    should_continue,
+    {
+        "delete": "delete_messages",
+        "chat": "chatbot"
+    }
+)
+graph.add_edge('chatbot', 'human_node')
+graph.add_edge("delete_messages", "research_node")
+graph.add_edge("research_node", END)
 app = graph.compile()
 
 # Optional: Save graph visualization
 with open("graph.png", "wb") as f:
     f.write(app.get_graph().draw_mermaid_png())
 
-
-user_input = input("Enter a Topic: ")
-system_prompt = "from the following Topic generate 5 question that will help expand on the topic given. -only return the 5 question. topic: "
-
-state = app.invoke({'system_prompt': system_prompt,'topic': user_input})
-#print(state)
-
+result = app.invoke({})
+print(result)
